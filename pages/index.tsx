daef54c5a943f7c19bbb701d61966b0369bc97c0
mrchristian17 <dcwrath17@gmail.com>
2022-09-22 22:12:05 -0600

    Installed dependencies and added header with my icons that are links to my social media sites

---
+++ pages/index.tsx
@@ -0,0 +1,25 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import Header from '../components/Header'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Christian Portfolio</title>
+      </Head>
+
+      {/* Header */}
+      <Header />
+      {/* Hero */}
+      {/* About */}
+      {/* Experience */}
+      {/* Skills */}
+      {/* Projects */}
+      {/* Contact Me */}
+    </div>
+  )
+}
+
+export default Home
